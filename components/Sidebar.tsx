d0ed247a489bd75919658f65a04b2d22b23b90a5
Ashvin Jadhao <ajadhao@altimetrik.com>
2026-03-15 20:09:51 -0400

    initial commit

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,18 +8,18 @@ return(
 <a href="/tools/word-counter">Word Counter</a>
 <a href="/tools/case-converter">Case Converter</a>
 <a href="/tools/duplicate-lines">Duplicate Lines</a>
-<a href="/tools/text-cleaner">Text Cleaner</a>
-<a href="/tools/reverse-text">Reverse Text</a>
-<a href="/tools/slug-generator">Slug Generator</a>
-<a href="/tools/password-generator">Password Generator</a>
-<a href="/tools/reading-time">Reading Time</a>
-<a href="/tools/json-formatter">Json Formatter</a>
-<a href="/tools/json-minifier">Json Minifier</a>
-<a href="/tools/base64-encode">Base64 Encode</a>
-<a href="/tools/base64-decode">Base64 Decode</a>
-<a href="/tools/url-encode">Url Encode</a>
-<a href="/tools/url-decode">Url Decode</a>
-<a href="/tools/random-string">Random String</a>
+{/*<a href="/tools/text-cleaner">Text Cleaner</a>*/}
+{/*<a href="/tools/reverse-text">Reverse Text</a>*/}
+{/*<a href="/tools/slug-generator">Slug Generator</a>*/}
+{/*<a href="/tools/password-generator">Password Generator</a>*/}
+{/*<a href="/tools/reading-time">Reading Time</a>*/}
+{/*<a href="/tools/json-formatter">Json Formatter</a>*/}
+{/*<a href="/tools/json-minifier">Json Minifier</a>*/}
+{/*<a href="/tools/base64-encode">Base64 Encode</a>*/}
+{/*<a href="/tools/base64-decode">Base64 Decode</a>*/}
+{/*<a href="/tools/url-encode">Url Encode</a>*/}
+{/*<a href="/tools/url-decode">Url Decode</a>*/}
+{/*<a href="/tools/random-string">Random String</a>*/}
 </div>
 )
 }
