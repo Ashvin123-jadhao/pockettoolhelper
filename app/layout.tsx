460833010b4559e24aa3a64ffa73fbc3f61048f8
Ashvin Jadhao <ajadhao@altimetrik.com>
2026-03-18 21:01:24 -0400

    Adding speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import Sidebar from '../components/Sidebar'
 import Header from '../components/Header'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({children}){
 return(
@@ -14,6 +15,7 @@ return(
 <Header/>
 {children}
     <Analytics />
+    <SpeedInsights />
 </div>
 </div>
 </body>
